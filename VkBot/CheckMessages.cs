6e8c7d679fc6312396308c4f15070fff541eb4ba
konovalov2k@mail.ru <konovalov2k@mail.ru>
2023-10-15 11:02:30 +0500

    пока что нормально работает только афторизация

---
+++ VkBot/CheckMessages.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet.AudioBypassService.Extensions;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet;
+
+namespace VkBot
+{
+    public class CheckMessages
+    {
+        private List<string> finalMessages = new List<string> {"","",""};
+        public void Start(VkApi api)
+        {
+            while (true)
+            {
+                var getHistory = api.Messages.GetHistory(new MessagesGetHistoryParams
+                {
+                    PeerId = -182985865,
+                    //UserId = 178549456,
+                    Offset = 0,
+                    Reversed = false,
+                    Count = 3
+                });
+
+                var newMessages = new List<string>();
+
+                foreach (var message in getHistory.Messages)
+                {
+                    newMessages.Add(message.Text);
+                }
+
+                if (newMessages[0] != finalMessages[0] && newMessages[1] != finalMessages[1] && newMessages[2] != finalMessages[2])
+                {
+                    finalMessages[0] = newMessages[0];
+                    finalMessages[1] = newMessages[1];
+                    finalMessages[2] = newMessages[2];
+                    for (int i = newMessages.Count - 1; i==0;i--)
+                    {
+                        Console.WriteLine("==============================================");
+                        Console.WriteLine(finalMessages[i]);
+                    }
+                    //Console.WriteLine("==============================================");
+                    //Console.WriteLine(newMessage);
+                }
+
+
+
+                System.Threading.Thread.Sleep(3000);
+            }
+        }
+
+
+        
+    }
+}
--- VkBot/Program.cs
+++ VkBot/Program.cs
@@ -4,49 +4,97 @@ using VkNet.Model;
 using VkNet;
 using Microsoft.Extensions.DependencyInjection;
 using VkNet.Enums;
+using System.IO;
+using VkNet.Exception;
 
 namespace VkBot
 {
     internal class Program
     {
 
-        static readonly HttpClient client = new HttpClient();
-
-        
-
         static async Task Main()
         {
+            StreamReader sr = new StreamReader("token.txt");
+            string token = sr.ReadToEnd();
+            sr.Close();
 
             var user = new UserData();
 
             var api = new VkApi();
 
-            api.Authorize(new ApiAuthParams
+            try
             {
-                //id приложения kate mobile
-                ApplicationId = 2685278,
-                Login = user.Login,
-                Password = user.Password,
-                Settings = Settings.All
-
-            });
-            Console.WriteLine(api.Token);
-            //var res = api.Groups.Get(new GroupsGetParams());
-
-            //Console.WriteLine(res.TotalCount);
-
-            //var longPoll = api.Messages.GetLongPollServer(needPts: true);
-            //var serv = api.Messages.GetLongPollHistory(new MessagesGetLongPollHistoryParams() { Pts = longPoll.Pts, Ts = longPoll.Ts });
-
-            //Console.WriteLine(serv.Messages);
-
-            var getHistory = api.Messages.GetHistory(new MessagesGetHistoryParams
+                api.Authorize(new ApiAuthParams
+                {
+                    AccessToken = token
+                });
+                var res = api.Groups.Get(new GroupsGetParams());
+                Console.WriteLine(res.TotalCount);
+            }
+            catch (UserAuthorizationFailException)
             {
-                PeerId = -182985865,
-                Offset = 1
-            });
+                api.Authorize(new ApiAuthParams
+                {
+                    //id приложения kate mobile
+                    ApplicationId = 2685278,
+                    Login = user.Login,
+                    Password = user.Password,
+                    Settings = Settings.All
 
-            Console.WriteLine(getHistory);
+                });
+                StreamWriter sw = new StreamWriter("token.txt");
+                sw.WriteLine(api.Token);
+                sw.Close();
+            }
+
+            //var checkMess = new CheckMessages();
+            //checkMess.Start(api);
+
+            //var getLongPollServer = api.Messages.GetLongPollServer(true);
+
+            //var pts = getLongPollServer.Pts;
+            //var ts = getLongPollServer.Ts;
+
+            //var getLongPollHistory = api.Messages.GetLongPollHistory(new MessagesGetLongPollHistoryParams
+            //{
+            //    Ts = ts,
+            //    Pts = pts,
+            //    PreviewLength = 0,
+            //    //Onlines = true, // возвращать сообщения от пользователей которые онлайн
+            //    //Fields = UsersFields.Nickname,
+            //    //GroupId = -182985865,
+            //    EventsLimit = 1000,
+            //    MsgsLimit = 200
+
+            //});
+
+            //foreach (var message in getLongPollHistory.Messages)
+            //{
+            //    Console.WriteLine(string.Format("Дата и время: {0}, Сообщение: {1}\r\n", message.Date, message.Body));
+
+            //}
+
+
+
+            //var getHistory = api.Messages.GetHistory(new MessagesGetHistoryParams
+            //{
+            //    PeerId = -182985865,
+            //    //UserId = ,
+            //    Offset = 0,
+            //    Reversed = false,
+            //    Count = 3
+            //});
+
+            //var messages = new Stack<string>();
+
+            //foreach (var message in getHistory.Messages)
+            //{
+            //    messages.Push(message.Text);
+            //}
+            //while (messages.Count > 0)
+            //{
+            //    Console.WriteLine("Сообщение: " + messages.Pop());
+            //}
 
             //api.Messages.Send(new VkNet.Model.MessagesSendParams
             //{
